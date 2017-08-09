07a2875ac9d26a5e06ede85562532eb69a3b597e
chriseth <chris@ethereum.org>
2017-08-09 11:20:13 +0200

    callgraph wip

--- libsolidity/analysis/CallgraphBuilder.cpp
+++ libsolidity/analysis/CallgraphBuilder.cpp
@@ -103,107 +103,332 @@ public:
 	virtual void endVisit(InlineAssembly const& _node) override {}
 	virtual void endVisit(Break const& _node) override {}
 	virtual void endVisit(Return const& _node) override {}
-	virtual void endVisit(Throw const& _node) override {}
-	virtual void endVisit(VariableDeclarationStatement const& _node) override {}
-	virtual void endVisit(ExpressionStatement const& _node) override {}
-	virtual void endVisit(Conditional const& _node) override {}
-	virtual void endVisit(Assignment const& _node) override {}
-	virtual void endVisit(TupleExpression const& _node) override {}
-	virtual void endVisit(UnaryOperation const& _node) override {}
-	virtual void endVisit(BinaryOperation const& _node) override {}
-	virtual void endVisit(FunctionCall const& _node) override {}
-	virtual void endVisit(NewExpression const& _node) override {}
-	virtual void endVisit(MemberAccess const& _node) override {}
+	virtual void endVisit(Throw const& _node) override
+	{
+		m_nodes[&_node].hasSideEffects = true;
+	}
+	virtual void endVisit(VariableDeclarationStatement const& _variableDeclarationStatement) override
+	{
+		m_nodes[&_variableDeclarationStatement];
+		if (Expression const* expression = _variableDeclarationStatement.initialValue())
+		{
+			CallgraphNode& node = m_nodes[&_variableDeclarationStatement] = m_nodes[expression];
+			TypePointers valueTypes;
+			if (auto tupleType = dynamic_cast<TupleType const*>(expression->annotation().type.get()))
+				valueTypes = tupleType->components();
+			else
+				valueTypes = TypePointers{expression->annotation().type};
+			auto const& assignments = _variableDeclarationStatement.annotation().assignments;
+			solAssert(assignments.size() == valueTypes.size(), "");
+			for (size_t i = 0; i < assignments.size(); ++i)
+			{
+				size_t j = assignments.size() - i - 1;
+				solAssert(!!valueTypes[j], "");
+				VariableDeclaration const* varDecl = assignments[j];
+				if (varDecl)
+					node = node + processAssignment(
+						varDecl,
+						*varDecl->annotation().type,
+						*valueTypes[j]
+					);
+			}
+		}
+	}
+	virtual void endVisit(ExpressionStatement const& _node) override
+	{
+		m_nodes[&_node] = m_nodes[_node.expression()];
+	}
+	virtual void endVisit(Conditional const& _node) override
+	{
+		// TODO control flow
+		m_nodes[&_node] =
+			m_nodes[&_node.condition()] +
+			m_nodes[&_node.trueExpression()] +
+			m_nodes[&_node.falseExpression()];
+	}
+	virtual void endVisit(Assignment const& _assignment) override
+	{
+		// TODO this could be more intelligent with tuples.
+		// TODO finding the variable that is assigned to could be made more intelligent
+		// (or whether it writes to storage or not)
+		m_nodes[&_assignment] =
+			m_nodes[&_assignment.leftHandSide()] +
+			m_nodes[&_assignment.rightHandSide()] +
+			processAssignment(
+				referencedVariable(_assignment.leftHandSide()),
+				*_assignment.leftHandSide().annotation().type,
+				*_assignment.rightHandSide().annotation().type
+			);
+	}
+	CallgraphNode processAssignment(VariableDeclaration const* leftHandSide, Type const& _leftType, Type const& _rightType)
+	{
+		CallgraphNode node;
+
+		if (_rightType.dataStoredIn(DataLocation::Storage))
+			node.readsStorage = true;
+
+		node.hasSideEffects = true;
+		node.writes.insert(var);
+
+		if (!_leftType.isValueType() && !_leftType.dataStoredIn(DataLocation::Storage))
+		{
+			// We know it does not write to storage.
+		}
+		else if (!var || var->isStateVariable())
+			node.writesStorage = true;
+		return node;
+	}
+	virtual void endVisit(TupleExpression const& _tuple) override
+	{
+		auto& node = m_nodes[&_tuple];
+		for (auto const& component: _tuple.components())
+			if (component)
+			{
+				node = node + m_nodes[&component];
+				if (component->annotation().type->dataStoredIn(DataLocation::Storage))
+					node.readsStorage = true;
+			}
+	}
+	virtual void endVisit(UnaryOperation const& _unaryOperation) override
+	{
+		m_nodes[&_unaryOperation] = m_nodes[&_unaryOperation.subExpression()];
+		if (_unaryOperation.annotation().type->category() == Type::Category::RationalNumber)
+			return;
+
+		switch (_unaryOperation.getOperator())
+		{
+		case Token::Not:
+		case Token::BitNot:
+		case Token::Add:
+		case Token::Sub:
+			return;
+		case Token::Delete:
+		case Token::Inc:
+		case Token::Dec:
+		{
+			m_nodes[&_unaryOperation].hasSideEffects = true;
+			// TODO this could be made more intelligent, especially in connecting to
+			// e.g. deleting members of storage or memory structs.
+			VariableDeclaration const* var = referencedVariable(_unaryOperation.subExpression());
+			m_nodes[&_unaryOperation].writes.insert(var);
+			auto t = _unaryOperation.subExpression().annotation().type;
+			if (!t->isValueType() && !t->dataStoredIn(DataLocation::Storage))
+			{
+				// We know it does not write to storage.
+			}
+			else if (!var || var->isStateVariable())
+				m_nodes[&_unaryOperation].writesStorage = true;
+			break;
+		default:
+			solAssert(false, "Invalid unary operator: " + string(Token::toString(_unaryOperation.getOperator())));
+		}
+		}
+	}
+	virtual void endVisit(BinaryOperation const& _binaryOperation) override
+	{
+		Token::Value const c_op = _binaryOperation.getOperator();
+		if (c_op == Token::And || c_op == Token::Or)
+		{
+			// TODO short-circuiting!
+		}
+
+		m_nodes[&_binaryOperation] =
+			m_nodes[&_binaryOperation.leftExpression()] +
+			m_nodes[&_binaryOperation.rightExpression()];
+	}
+	virtual void endVisit(FunctionCall const& _functionCall) override
+	{
+		CallgraphNode n = m_nodes[&_functionCall.expression()];
+		for (auto const& arg: _functionCall.arguments())
+			n = n + m_nodes[arg.get()];
+		CallgraphNode& node = m_nodes[&_functionCall];
+		node = n;
+
+		for (auto const& arg: _functionCall.arguments())
+			if (arg->annotation().type->dataStoredIn(DataLocation::Storage))
+				node.readsStorage = true;
+		if (auto const* ft = dynamic_cast<FunctionType const*>(_functionCall.expression().annotation().type.get()))
+			if (ft->bound() && _functionCall.expression().annotation().type->dataStoredIn(DataLocation::Storage))
+				node.readsStorage = true;
+
+		if (_functionCall.annotation().kind != FunctionCallKind::FunctionCall)
+			return;
+
+		// just about anything might happen due to reentrancy, event "sends value".
+		// What cannot happen is that local variables are written to.
+		bool affectStateArbitrarily = false;
+		bool hasSideEffects = true;
+		bool calls = false;
+
+		FunctionType const& function = dynamic_cast<FunctionType const&>(*_functionCall.expression().annotation().type);
+		switch (function.kind())
+		{
+		case FunctionType::Kind::Internal:
+		{
+			affectStateArbitrarily = !function.isConstant();
+			calls = true;
+			break;
+		}
+		case FunctionType::Kind::External:
+		case FunctionType::Kind::CallCode:
+		case FunctionType::Kind::DelegateCall:
+		case FunctionType::Kind::BareCall:
+		case FunctionType::Kind::BareCallCode:
+		case FunctionType::Kind::BareDelegateCall:
+		case FunctionType::Kind::Creation:
+		{
+			affectStateArbitrarily = true;
+			calls = true;
+			break;
+		}
+		case FunctionType::Kind::Send:
+		case FunctionType::Kind::Transfer:
+		{
+			calls = true;
+			node.sendsValue = true;
+			break;
+		}
+		case FunctionType::Kind::Selfdestruct:
+		{
+			calls = true;
+			node.sendsValue = true;
+			node.selfdestructs = true;
+			break;
+		}
+		case FunctionType::Kind::Revert:
+		case FunctionType::Kind::Assert:
+		case FunctionType::Kind::Require:
+		{
+			// TODO model control flow here.
+			hasSideEffects = false;
+			break;
+		}
+		case FunctionType::Kind::Log0:
+		case FunctionType::Kind::Log1:
+		case FunctionType::Kind::Log2:
+		case FunctionType::Kind::Log3:
+		case FunctionType::Kind::Log4:
+		case FunctionType::Kind::Event:
+		{
+			node.writesLogs = true;
+			break;
+		}
+		case FunctionType::Kind::BlockHash:
+		{
+			hasSideEffects = false;
+			node.readsEnvironment = true;
+			break;
+		}
+		case FunctionType::Kind::ECRecover:
+		case FunctionType::Kind::SHA256:
+		case FunctionType::Kind::RIPEMD160:
+		{
+			calls = true;
+			hasSideEffects = false;
+			break;
+		}
+		case FunctionType::Kind::SetGas:
+		case FunctionType::Kind::SetValue:
+		case FunctionType::Kind::SHA3:
+		case FunctionType::Kind::AddMod:
+		case FunctionType::Kind::MulMod:
+		case FunctionType::Kind::ObjectCreation:
+		{
+			hasSideEffects = false;
+			break;
+		}
+		case FunctionType::Kind::ByteArrayPush:
+		case FunctionType::Kind::ArrayPush:
+		{
+			// TODO determine the exact effects,
+			// this might only write to a single variable.
+			affectStateArbitrarily = true;
+			break;
+		}
+		default:
+			solAssert(false, "Invalid function type.");
+		}
+
+		if (hasSideEffects)
+			node.hasSideEffects = true;
+		if (calls)
+			node.calls = true;
+		if (affectStateArbitrarily)
+		{
+			CallgraphNode& node = m_nodes[&_functionCall];
+			node.hasSideEffects = true;
+			node.readsEnvironment = true;
+			// TODO does not read or write local variables
+			node.reads.insert(nullptr);
+			node.writes.insert(nullptr);
+			node.readsStorage = true;
+			node.selfdestructs = true;
+			node.sendsValue = true;
+			node.writesLogs = true;
+			node.writesStorage = true;
+		}
+	}
+	virtual void endVisit(NewExpression const& _node) override
+	{
+		m_nodes[&_node] = m_nodes[&_node.typeName()];
+	}
+	virtual void endVisit(MemberAccess const& _memberAccess) override
+	{
+		m_nodes[&_memberAccess] = m_nodes[&_memberAccess.expression()];
+
+		if (auto const& varDecl = dynamic_cast<VariableDeclaration const*>(_memberAccess.annotation().referencedDeclaration))
+			m_nodes[&_memberAccess] += variableWasReferenced(*varDecl);
+
+		ASTString const& member = _memberAccess.memberName();
+		switch (_memberAccess.expression().annotation().type->category())
+		{
+		case Type::Category::Contract:
+		case Type::Category::Integer:
+			if (member == "balance" && !_memberAccess.annotation().referencedDeclaration)
+				m_nodes[&_memberAccess].readsEnvironment = true;
+			break;
+		case Type::Category::Magic:
+			// we can ignore the kind of magic and only look at the name of the member
+			if (member != "data" && member != "sig")
+				m_nodes[&_memberAccess].readsEnvironment = true;
+			break;
+		case Type::Category::Struct:
+		{
+			if (
+				_memberAccess.expression().annotation().type->dataStoredIn(DataLocation::Storage) &&
+				_memberAccess.annotation().type->isValueType()
+			)
+				m_nodes[&_memberAccess].readsStorage = true;
+			break;
+		}
+		case Type::Category::Array:
+		{
+			auto const& type = dynamic_cast<ArrayType const&>(*_memberAccess.expression().annotation().type);
+			if (member == "length" && type.isDynamicallySized() && type.dataStoredIn(DataLocation::Storage))
+				m_nodes[&_memberAccess].readsStorage = true;
+			break;
+		}
+		}
+	}
 	virtual void endVisit(IndexAccess const& _indexAccess) override
 	{
 		solAssert(_indexAccess.indexExpression(), "");
-		m_nodes[&_indexAccess] = m_nodes[_indexAccess.baseExpression()] + m_nodes[_indexAccess.indexExpression()];
+		m_nodes[&_indexAccess] = m_nodes[&_indexAccess.baseExpression()] + m_nodes[_indexAccess.indexExpression()];
 
 		Type const& baseType = *_indexAccess.baseExpression().annotation().type;
 
-		if (baseType.category() == Type::Category::Mapping)
-		{
-			solAssert(m_nodes[&_indexAccess].readsStorage, "");
-		}
-		else if (baseType.category() == Type::Category::Array)
-		{
-			ArrayType const& arrayType = dynamic_cast<ArrayType const&>(baseType);
-			solAssert(_indexAccess.indexExpression(), "Index expression expected.");
-
-			_indexAccess.indexExpression()->accept(*this);
-			utils().convertType(*_indexAccess.indexExpression()->annotation().type, IntegerType(256), true);
-			// stack layout: <base_ref> [<length>] <index>
-			ArrayUtils(m_context).accessIndex(arrayType);
-			switch (arrayType.location())
-			{
-			case DataLocation::Storage:
-				if (arrayType.isByteArray())
-				{
-					solAssert(!arrayType.isString(), "Index access to string is not allowed.");
-					setLValue<StorageByteArrayElement>(_indexAccess);
-				}
-				else
-					setLValueToStorageItem(_indexAccess);
-				break;
-			case DataLocation::Memory:
-				setLValue<MemoryItem>(_indexAccess, *_indexAccess.annotation().type, !arrayType.isByteArray());
-				break;
-			case DataLocation::CallData:
-				//@todo if we implement this, the value in calldata has to be added to the base offset
-				solUnimplementedAssert(!arrayType.baseType()->isDynamicallySized(), "Nested arrays not yet implemented.");
-				if (arrayType.baseType()->isValueType())
-					CompilerUtils(m_context).loadFromMemoryDynamic(
-						*arrayType.baseType(),
-						true,
-						!arrayType.isByteArray(),
-						false
-					);
-				break;
-			}
-		}
-		else if (baseType.category() == Type::Category::FixedBytes)
-		{
-			FixedBytesType const& fixedBytesType = dynamic_cast<FixedBytesType const&>(baseType);
-			solAssert(_indexAccess.indexExpression(), "Index expression expected.");
-
-			_indexAccess.indexExpression()->accept(*this);
-			utils().convertType(*_indexAccess.indexExpression()->annotation().type, IntegerType(256), true);
-			// stack layout: <value> <index>
-			// check out-of-bounds access
-			m_context << u256(fixedBytesType.numBytes());
-			m_context << Instruction::DUP2 << Instruction::LT << Instruction::ISZERO;
-			// out-of-bounds access throws exception
-			m_context.appendConditionalInvalid();
-
-			m_context << Instruction::BYTE;
-			utils().leftShiftNumberOnStack(256 - 8);
-		}
-		else if (baseType.category() == Type::Category::TypeType)
-		{
-			solAssert(baseType.sizeOnStack() == 0, "");
-			solAssert(_indexAccess.annotation().type->sizeOnStack() == 0, "");
-			// no-op - this seems to be a lone array type (`structType[];`)
-		}
-		else
-			solAssert(false, "Index access only allowed for mappings or arrays.");
-
-		return false;
+		if (baseType.dataStoredIn(DataLocation::Storage) && _indexAccess.annotation().type->isValueType())
+			m_nodes[&_indexAccess].readsStorage = true;
 	}
 	virtual void endVisit(Identifier const& _identifier) override
 	{
 		Declaration const* declaration = _identifier.annotation().referencedDeclaration;
 		solAssert(declaration, "");
-		m_nodes[&_identifier].reads.insert(declaration);
-		// TODO "lValueRequested" is not strict enough for passing storage or memory pointers.
-		if (_identifier.annotation().lValueRequested)
-			m_nodes[&_identifier].writes.insert(declaration);
-		if (declaration->type()->dataStoredIn(DataLocation::Storage))
-		{
-			// TODO "lValueRequested" is not strict enough for passing storage or memory pointers.
-			m_nodes[&_identifier].writesStorage = _identifier.annotation().lValueRequested;
-			m_nodes[&_identifier].readsStorage = true;
-		}
 
+		if (VariableDeclaration const* varDecl = dynamic_cast<VariableDeclaration const*>(declaration))
+		{
+			m_nodes[&_identifier] = variableWasReferenced(*varDecl);
+		}
 		if (MagicVariableDeclaration const* magicVar = dynamic_cast<MagicVariableDeclaration const*>(declaration))
 		{
 			switch (magicVar->type()->category())
@@ -211,6 +436,7 @@ public:
 			case Type::Category::Contract:
 				// "this" or "super"
 				if (!dynamic_cast<ContractType const&>(*magicVar->type()).isSuper())
+					// reads the address
 					m_nodes[&_identifier].readsEnvironment = true;
 				break;
 			case Type::Category::Integer:
@@ -226,6 +452,31 @@ public:
 	virtual void endVisit(Literal const& _node) override { m_nodes[&_node]; }
 
 private:
+
+	/// @returns the variable referenced in an lvalue expression - if any.
+	VariableDeclaration const* referencedVariable(Expression const& _expression)
+	{
+		Declaration const* declaration = nullptr;
+		if (Identifier const* identifier = dynamic_cast<Identifier const*>(&_expression))
+			declaration = identifier->annotation().referencedDeclaration;
+		else if (MemberAccess const* memberAccess = dynamic_cast<MemberAccess const*>(&_expression))
+			declaration = memberAccess->annotation().referencedDeclaration;
+		return dynamic_cast<VariableDeclaration const*>(declaration);
+	}
+
+	/// Used by identifiers or member access nodes that access a variable.
+	CallgraphNode variableWasReferenced(VariableDeclaration const& _varDecl)
+	{
+		CallgraphNode r;
+		r.reads.insert(_varDecl);
+		if (_varDecl->isStateVariable() && !_varDecl->isConstant() && _varDecl.type()->isValueType())
+			r.readsStorage = true;
+		// Reference types do not really read from storage at this point.
+		return r;
+	}
+
+
+
 	std::map<ASTNode const*, CallgraphNode>& m_nodes;
 };
 
